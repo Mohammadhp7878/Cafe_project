0f0b8baaab38d98accbc48bf591f527d95aa9026
mohammad <mohammadhp858@gmail.com>
2023-06-25 23:11:01 +0330

    bug fix config

--- cafe/settings.py
+++ cafe/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ym@b1!o2we=unm3hm29@7l%v-2=es%c(3_cfn3%#*9ehjk!y*4'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
