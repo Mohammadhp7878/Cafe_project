0e84740e92a6402156220e9878043995295c2a6f
mohammad <mohammadhp858@gmail.com>
2023-06-26 19:48:11 +0330

    models for table, reciept, category

--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+import re
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
 
@@ -38,3 +40,30 @@ class User(AbstractBaseUser):
     is_admin = models.BooleanField(default=False)
     user_is_staff = models.BooleanField(default=False)
     user_is_superadmin = models.BooleanField(default=False)
+
+
+
+def validate_password(value):
+    if len(value) < 8 or len(value) > 20:
+        raise ValidationError('Password must be between 8 and 20 characters.')
+    if not re.search(r'[A-Z]', value):
+        raise ValidationError('Password must have at least one capital letter.')
+    if not re.search(r'\d', value):
+        raise ValidationError('Password must have at least one number.')
+    if not re.search(r'[!@#$%^&*()-_=+{}<>]', value):
+        raise ValidationError('Password must have at least one special character.')
+
+
+def validate_birth_date(value):
+    if value.year <= 2008:
+        raise ValidationError('Date of birth must be after 2008.')
+
+
+class UserAnonymous(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone = models.PositiveIntegerField()
+    email = models.EmailField()
+    password = models.CharField(max_length=20, validators=[validate_password])
+    user_image_src = models.CharField(max_length=250)
+    birth_date = models.DateField(validators=[validate_birth_date])
--- products/models.py
+++ products/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
-class product(models.Model):
+class Category(models.Model):
+    category_name = models.CharField(max_length=50)
+    
+class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.PositiveBigIntegerField(max_length=20)
-    category = ...
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
     discount = models.PositiveSmallIntegerField(max_length=2)
     serving_time = models.DurationField()
     estimated_cooking_time = models.DurationField()
-    
+
