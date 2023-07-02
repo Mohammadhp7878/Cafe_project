868f3a51355d5a0f5435f13a4c3c1a5835e126c5
Abbas moradi <en.moradi.66@gmail.com>
2023-07-02 10:17:59 +0330

    add the save in session method

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.views import View
 from django.shortcuts import render
+from django.shortcuts import redirect
 from .models import Product
 
 
@@ -7,4 +8,11 @@ class HomeView(View):
     def get(self, request):
         products = Product.objects.all()
         return render(request, 'base.html', {'products': products})
+    
+    def post(self, request):
+        product_id = request.POST.get('id')
+        cart_value = [request.session.get('cart', 0)]
+        cart_value.append(product_id)
+        request.session['cart'] = cart_value
+        return redirect('product')
 
