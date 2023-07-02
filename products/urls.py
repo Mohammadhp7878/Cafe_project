5209114de7d765c74d0d87cffb2203adffe474f0
Abbas moradi <en.moradi.66@gmail.com>
2023-07-02 11:06:50 +0330

    change the view name

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import HomeView
+from .views import ProductView
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
+    path('', ProductView.as_view(), name='home'),
 ]
