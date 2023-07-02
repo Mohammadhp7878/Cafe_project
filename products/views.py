effc69eecde416e0a8d39d1a84c06872445d2f4c
Abbas moradi <en.moradi.66@gmail.com>
2023-07-02 18:23:14 +0330

    add the category class view

--- products/views.py
+++ products/views.py
@@ -7,8 +7,7 @@ from .models import Product, Category
 class ProductView(View):
     def get(self, request):
         products = Product.objects.all()
-        categories = Category.objects.all()
-        return render(request, 'product/new_product.html', {'products': products, 'categories': categories})
+        return render(request, 'product/new_product.html', {'products': products})
     
     def post(self, request):
         product_id = request.POST.get('id')
@@ -16,4 +15,10 @@ class ProductView(View):
         cart_value.append(product_id)
         request.session['cart'] = cart_value
         return redirect('product')
+
+
+class CategoryView(View):
+    def get(self, request):
+        categories = Category.objects.all()
+        return render(request, 'product/new_product.html', {'categories': categories})
 
