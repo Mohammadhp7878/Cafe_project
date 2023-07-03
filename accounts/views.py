9daef1d5f3e3ff130843041ddf63648e1e46830f
mohammad <mohammadhp858@gmail.com>
2023-07-03 14:45:21 +0330

    add raw view for cashier login

--- accounts/views.py
+++ accounts/views.py
@@ -1,2 +1,18 @@
 from django.shortcuts import render
+from django.views import View
+from .forms import LoginForm
+
+class CashierLogin(View):
+    loginform = LoginForm
+    template = 'login.html'
+
+    def get(self, request):
+        form = self.loginform(request)
+        return render(request, self.template, {'form': form})
+
+    def post(self, request):
+        form = self.loginform(request, data=request.POST)
+        if form.is_valid():
+            ...
+        return render(request, self.template)
 
