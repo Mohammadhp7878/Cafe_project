06ab61a58a963deec34b781a5f693a3bbd4b49ce
sakinehM93 <sakineh75pourbakhshali@gmail.com>
2023-07-04 10:35:50 +0430

    add cashier url

--- cafe/urls.py
+++ cafe/urls.py
@@ -22,7 +22,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('products.urls')),
-    path('', include('accounts.urls'))
+    path('', include('accounts.urls')),
+    path('', include('cashier.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
