cc50d8621a3bf8a41de57ac8f9d92093071075fd
sakinehM93 <sakineh75pourbakhshali@gmail.com>
2023-07-04 10:09:57 +0430

    add dashboard view

--- accounts/views.py
+++ accounts/views.py
@@ -1,31 +1,8 @@
 from django.views import View
-from django.shortcuts import render, redirect
-from orders.models import Order
+from django.shortcuts import render
 from .forms import LoginForm, OrderForm
 from .models import User
 from django.contrib import messages
-
-
-class DashboardView(View):
-    def get(self, request):
-        orders = Order.objects.all()
-        # customer = Customer.objects.all()
-        total_order = orders.count()
-        deliver = orders.filter(status='Delivered').count()
-        pending = orders.filter(status='Pending').count()
-        cooking = orders.filter(status='Cooking').count()
-        send_to_kitchen = orders.filter(status='Sending').count()
-
-        context = {
-            'orders': orders,
-            # 'customers': customer,
-            'total_order': total_order,
-            'deliver': deliver,
-            'pending': pending,
-            'cooking': cooking,
-            'send_to_kitchen': send_to_kitchen,
-        }
-        return render(request, 'dashboard.html', context)
 
 
 class CashierLogin(View):
