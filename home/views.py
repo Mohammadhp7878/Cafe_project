9ef3176e0aff9187e15a91f782a66e7c0569383e
mohammad <mohammadhp858@gmail.com>
2023-07-10 21:49:18 +0330

    changing templates

--- home/views.py
+++ home/views.py
@@ -1,4 +1,4 @@
-from typing import Any
+from django.db.models import Sum
 from django.db.models.query import QuerySet
 from django.shortcuts import render
 from . import models
@@ -17,6 +17,7 @@ class SearchProduct(ListView):
 
     def get_queryset(self):
         search_products = self.request.GET.get('search_product')
+        top_selling = top_selling_products = Product.objects.annotate(total_quantity=Sum('product_order__number')).order_by('-total_quantity')[:10]
         if search_products:
             products = Product.objects.filter(name__icontains = search_products)
             return products
