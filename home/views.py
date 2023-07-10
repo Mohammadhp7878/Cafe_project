959dde3aaee31604b30b80e2bb8c09e5755b0158
mohammad <mohammadhp858@gmail.com>
2023-07-10 16:13:22 +0330

    create view to handle search result

--- home/views.py
+++ home/views.py
@@ -1,7 +1,22 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from . import models
-
+from django.views.generic import ListView
+from products.models import Product
 
 def home(request):
     about_us = models.AboutUs.objects.order_by('-id').first()
     return render(request, 'base.html', {'about_us': about_us})
+
+
+class SearchProduct(ListView):
+    model = Product
+    template_name = 'search.html'
+    context_object_name = 'products'
+
+    def get_queryset(self):
+        search_products = self.request.GET.get('search_product')
+        if search_products:
+            products = Product.objects.filter(name__icontains = search_products)
+            return products
