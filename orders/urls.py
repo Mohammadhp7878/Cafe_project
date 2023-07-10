36ce1a7f0531fb78eac9d4fede50a91dcb0982a9
Ahmadreza Esfandiari <a.esfandiari.didex@gmail.com>
2023-07-10 21:00:20 +0330

    updating urls.py in orders

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from views import RemoveFromCartView
 
 urlpatterns = [
     path('cart/', views.CartView.as_view(), name='cart_view'),
+    path('cart/remove/<int:product_id>/', RemoveFromCartView.as_view(), name='remove_from_cart'),
 ]
