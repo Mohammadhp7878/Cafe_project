8dc8e4502f7dcb6cbc95bedf4a2dd129b81c8992
Abbas moradi <en.moradi.66@gmail.com>
2023-07-11 11:16:39 +0330

    Completing the view cookie

--- orders/views.py
+++ orders/views.py
@@ -3,11 +3,20 @@ from django.views import View
 from django.http import HttpResponse
 from django.utils.http import urlencode
 from django.conf import settings
-import json
+from .models import Product
 
 class CartView(View):
     def get(self, request):
-        
+        cart_product = request.COOKIES.get('cart')
+        number_of_product = {}
+        for i in cart_product:
+            if i in number_of_product:
+                number_of_product[i] += 1
+            else:
+                number_of_product[i] = int(1)
+        number_of_product.pop(',')
+        for key in number_of_product.keys():
+            products = Product.objects.filter(id=int(key))
         return render(request, 'cart_page.html')
 
 class RemoveFromCartView(View):
