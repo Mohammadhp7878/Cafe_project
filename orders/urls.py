7ab42701202d5bfeb4f23b7f8b70e3872e5ccb9e
Abbas moradi <en.moradi.66@gmail.com>
2023-07-12 22:11:37 +0330

    improve

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from views import RemoveFromCartView
+from .views import RemoveFromCartView
 
 urlpatterns = [
     path('cart/', views.CartView.as_view(), name='cart_view'),
